a05ee52faaf6a11dd3c37411906795caaafd5fdc
Kemal Abdillah <60648376+kemal003@users.noreply.github.com>
2021-11-30 01:24:46 +0700

    Kamera Setting

--- app/src/main/java/com/example/parktikom_admin/BuatPengumuman.kt
+++ app/src/main/java/com/example/parktikom_admin/BuatPengumuman.kt
@@ -1,15 +1,36 @@
 package com.example.parktikom_admin
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
 import com.example.parktikom_admin.databinding.ActivityBuatPengumumanBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.File
+import java.lang.Exception
 
 class BuatPengumuman : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private lateinit var storageRef : StorageReference
     private lateinit var binding: ActivityBuatPengumumanBinding
+    private lateinit var urlFoto: Uri
     private val testParkir = arrayOf(
         "parkir1",
         "parkir2"
@@ -21,6 +42,7 @@ class BuatPengumuman : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBuatPengumumanBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        storageRef = Firebase.storage.reference
 
         autoCompleteTextView = binding.autoCompleteTextView
         adapterItem = ArrayAdapter<String>(this, R.layout.item_parkir, testParkir)
@@ -31,5 +53,28 @@ class BuatPengumuman : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            try {
+                Glide.with(this).load(urlFoto).into(binding.takenPhoto)
+            } catch (e: Exception) {
+                Toast.makeText(this, "Batal mengambil foto", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.unggahFoto.setOnClickListener {
+            urlFoto = FileProvider.getUriForFile(
+                this,
+                "${this.packageName}.provider",
+                takenImage())
+
+            takePhoto.launch(urlFoto)
+        }
+    }
+
+    private fun takenImage() : File{
+        return File.createTempFile(
+            "IMG_",
+            "jpg",
+            getExternalFilesDir(Environment.DIRECTORY_PICTURES))
     }
 }
