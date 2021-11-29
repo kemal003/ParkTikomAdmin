b89bffdb1cb56e7611f922ee2b2f260c47a6204d
Kautsarratu <64664923+Kautsarratu@users.noreply.github.com>
2021-11-30 04:35:40 +0700

    update buanyakkk

---
+++ app/src/main/java/com/example/parktikom_admin/EditKuotaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.parktikom_admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditKuotaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_kuota)
+    }
+}
